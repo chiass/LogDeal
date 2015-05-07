74f7431302b1e8a21d7ab649f0a5b60af5fa6380
baidu <jiashishui>
2015-05-07 11:34:13 +0800

    commit

---
+++ src/com/baidu/duapp/Reviewer.java
@@ -0,0 +1,12 @@
+package com.baidu.duapp;
+
+/**
+ * Created by jiashishui on 15/5/7.
+ *
+ */
+public class Reviewer {
+
+    public static void main(String[] args) {
+        System.out.println("first class in baidu");
+    }
+}
